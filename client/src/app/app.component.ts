58b4d422a432f26965ac39d6b006c40b4ba084cb
KlugahKd <daniel.klugah@hubtel.com>
2024-03-20 19:52:19 +0000

    Creating angular project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'client';
